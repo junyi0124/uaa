77944621878f4ad0b97a4ed8f3a497577fc13816
junyi0124 <junyi0124@msn.com>
2021-04-09 16:19:49 +0800

    init

---
+++ UAA.Entity/UserAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UAA.Entity
+{
+    public class UserAccount
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(40)] 
+        public string DisplayName { get; set; }
+
+        [Required]
+        [StringLength(40)] 
+        public string Email { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public int Status { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdate { get; set; }
+
+    }
+}
---
+++ UAA.Model/AppException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UAA.Model
+{
+    public class AppException
+    {
+
+    }
+}
